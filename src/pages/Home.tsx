184932c342b7a8732fd1d2966ba1378699abe7a5
Brennan <48792128+btjl@users.noreply.github.com>
2023-04-16 18:38:55 +0800

    chore: configurations

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,17 @@
+import { AvatarImage } from "../assets";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="min-h-screen m-5 flex flex-col">
+      <h1 className="text-3xl mb-5">Brennan Tan</h1>
+      <p className="mb-5">
+        Hi, I'm Brennan. I am a Fullstack Software Engineer at GovTech Singapore
+        where I work on #TechForPublicGood.
+      </p>
+      <AvatarImage />
+      <p className="mt-5">[ To be filled ]</p>
+    </div>
+  );
 };
 
 export default HomePage;
